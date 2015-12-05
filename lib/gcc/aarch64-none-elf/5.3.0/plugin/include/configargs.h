5886cd9e85597c581e84e1a14f16fc22d63af2d3
Park Ju Hyung <qkrwngud825@gmail.com>
2015-12-05 15:57:27 +0900

    Import gcc-5.3.0.tar
    Configuration :
    export PATH=/home/arter97/linaro-64/bin:$PATH
    AR_FOR_TARGET=/home/arter97/linaro-64/aarch64-none-elf/bin/ar AS_FOR_TARGET=/home/arter97/linaro-64/aarch64-none-elf/bin/as LD_FOR_TARGET=/home/arter97/linaro-64/aarch64-none-elf/bin/ld NM_FOR_TARGET=/home/arter97/linaro-64/aarch64-none-elf/bin/nm OBJCOPY_FOR_TARGET=/home/arter97/linaro-64/aarch64-none-elf/bin/objcopy OBJDUMP_FOR_TARGET=/home/arter97/linaro-64/aarch64-none-elf/bin/objdump ranlib=/home/arter97/linaro-64/aarch64-none-elf/bin/ranlib STRIP_FOR_TARGET=/home/arter97/linaro-64/aarch64-none-elf/bin/strip \
    CFLAGS='-mtune=native' CXXFLAGS='-mtune=native' \
    /tmp/gcc/gcc-5.3.0/configure --prefix=/home/arter97/gcc-5.3-64 --host=x86_64-linux-gnu --build=x86_64-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --disable-ppl-version-check --disable-cloog-version-check --disable-isl-version-check --enable-cloog-backend=isl --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --disable-libssp --disable-nls --disable-libmudflap --disable-shared --disable-sjlj-exceptions --disable-libquadmath --disable-libitm --disable-libsanitizer --prefix=/home/arter97/gcc-5.3-64 --disable-docs --disable-nls --target=aarch64-none-elf --enable-threads --enable-tls --disable-libgomp --disable-libstdc__-v3 --disable-libitm --disable-libsanitizer --disable-libatomic --enable-plugins
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

---
+++ lib/gcc/aarch64-none-elf/5.3.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/tmp/gcc/gcc-5.3.0/configure --prefix=/home/arter97/gcc-5.3-64 --host=x86_64-linux-gnu --build=x86_64-linux-gnu --with-gnu-as --with-gnu-ld --enable-languages=c,c++ --disable-ppl-version-check --disable-cloog-version-check --disable-isl-version-check --enable-cloog-backend=isl --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --disable-libssp --disable-nls --disable-libmudflap --disable-shared --disable-sjlj-exceptions --disable-libquadmath --disable-libitm --disable-libsanitizer --prefix=/home/arter97/gcc-5.3-64 --disable-docs --disable-nls --target=aarch64-none-elf --enable-threads --enable-tls --disable-libgomp --disable-libstdc__-v3 --disable-libitm --disable-libsanitizer --disable-libatomic --enable-plugins";
+static const char thread_model[] = "single";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
